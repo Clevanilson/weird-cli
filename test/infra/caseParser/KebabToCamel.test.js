963918747ae859f7bfc1f5ff8c871caf17c583fb
Clevanilson <clevanilson.contato@gmail.com>
2024-05-01 18:10:57 -0300

    test(case-parser): fix import

--- test/infra/caseParser/KebabToCamel.test.js
+++ test/infra/caseParser/KebabToCamel.test.js
@@ -1,4 +1,4 @@
-const { KebabToCamel } = require("../../../src/infra/CaseParser");
+const { KebabToCamel } = require("../../../src/infra/caseParser");
 
 test("Should parse kebab-case some-prop to camelCase someProp", () => {
   expect(KebabToCamel.parse("some-prop")).toBe("someProp");
